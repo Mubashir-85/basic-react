8b73fa3c539e56c1d6563fbb71b9b304253630ea
Shaikh Mubashir Ahmed <shaikhmubashirahmed123@gmail.com>
2025-10-18 20:42:23 +0530

    use of usestate

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const incrementCount = () => {
+    if (count < 10) {
+      setCount(count + 1)
+    } else {
+      alert("You can't increment beyond 10")
+    }
+
+  }
+
+  const decrementCount = ()=>{
+      if (count > 0) {
+      setCount(count - 1)
+    } else {
+      alert("You can't decrement below 0")
+    }
+
+  }
+  
+
+  return (
+    <>
+      <button onClick={incrementCount}>
+        Increament
+      </button><br />
+      <h1>Count:{count}</h1>
+      <button onClick={decrementCount}>Decrease</button>
+
+    </>
+  )
+}
+
+export default App
