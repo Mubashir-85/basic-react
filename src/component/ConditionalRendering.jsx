adf429eae190ea1e3d7454be8203e50510af4161
Shaikh Mubashir Ahmed <shaikhmubashirahmed123@gmail.com>
2025-10-27 22:36:22 +0530

    Conditional Rendering using AND operator

--- src/component/ConditionalRendering.jsx
+++ src/component/ConditionalRendering.jsx
@@ -7,13 +7,20 @@ function ConditionalRendering() {
     <>
     <h1>Conditional Rendering Example</h1>
     <ul>
-        {fruits.length > 0 ? (    // this is conditional rendering used for enhance the performance
+        {fruits.length > 0 ? (    // this is conditional rendering used for enhance the performance. This is using ternary operator.
             fruits.map((fruit)=>(
                 <li key={fruit}>{fruit}</li>    
             ))
         ) : (
             <p>No fruits available</p>
         )}
+    </ul>
+
+    <h1>Example No : 2</h1>
+    <ul>
+        {fruits.length > 0 && fruits.map((fruit)=>(  //This is another way of conditional rendering using logical AND (&&) operator.  output will be same as above.
+            <li key={fruit}>{fruit}</li>
+        ))}
     </ul>
     </>
   )
