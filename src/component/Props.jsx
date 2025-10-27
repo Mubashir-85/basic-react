f89c21f3f8999fc05bb4004dd09552f04bf7ded4
Shaikh Mubashir Ahmed <shaikhmubashirahmed123@gmail.com>
2025-10-28 02:16:26 +0530

    Using Props

--- src/component/Props.jsx
+++ src/component/Props.jsx
@@ -4,7 +4,7 @@ function Props({color}) {
   return <>
   <h1>Colors</h1>
   {color.map((color)=>(
-    <li key={color}>{color}</li> // Here, we are using props to pass the colors array from App component to Props component and then using map method to display each color in a list item.
+    <li key={color}>{color}</li> // Here, we are using props to pass the colors array from App component to Props component and then using map method to display each color in a list item. and also with destructuring assignment to extract the color prop directly.
   ))}
   </>;
 }
